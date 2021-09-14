a7b60c3e96acd744a17638fd87dec063c83ca0dd
denm76 <denm76@gmail.com>
2021-09-14 17:14:26 +0300

    create first app

---
+++ women/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Главная страница')
+
+
+def index(request):
+    return HttpResponse("Страница приложения women")
+
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")
