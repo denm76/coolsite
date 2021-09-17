d470c60791af10aea752b4b15c88b97375a2e46c
denm76 <denm76@gmail.com>
2021-09-17 16:41:58 +0300

    Добавлена таблица Women

--- coolsite/urls.py
+++ coolsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from women import views
 
@@ -22,5 +24,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
 
 handler404 = views.pageNotFound
